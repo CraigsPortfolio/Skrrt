3e4b3a9c3b50b6021ae971993e0800beb950c882
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-03-22 14:45:45 +0000

    Main test

--- final/server.js
+++ final/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static('final'));
 app.get('/', function(req, res){
-  res.sendFile('final.html', {root: __dirname });
+  res.sendFile('main.html', {root: __dirname });
 });
 app.listen(8080);

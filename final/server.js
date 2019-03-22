8b3410604bd001ddcd0e07252c85cb40dc457997
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-03-22 14:58:20 +0000

    asdsa

--- final/server.js
+++ final/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.static('final'));
+app.use(express.static('final/css'));
 app.get('/', function(req, res){
   res.sendFile('css/style.css', {root: __dirname });
   res.sendFile('main.html', {root: __dirname });

388fafc524cb7cf0e9a4431cd733bbd28568c9f3
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-03-22 14:28:00 +0000

    Test

--- final/server.js
+++ final/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
-app.use(express.static('final'));
-
-app.get('/main', function(req, res){
- res.send("Hi "+name+" I am sure you will "+quest) ;
+app.get('/', function(req, res){
+ res.send("Hello world! by express”);
 });
+app.listen(8080); 

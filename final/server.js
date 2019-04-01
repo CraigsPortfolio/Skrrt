006f7b82406439b8b65c5cd2e4d29c715a25a3e1
1805675-CraigRobertson <1805675@rgu.ac.uk>
2019-04-01 16:58:47 +0100

    Added register page

--- final/server.js
+++ final/server.js
@@ -12,6 +12,10 @@ app.get('/main', function(req, res) {
  res.render('pages/main');
 });
 
+app.get('/register', function(req, res) {
+ res.render('pages/register');
+});
+
 
 app.listen(8080);
 console.log('8080 is the magic port');

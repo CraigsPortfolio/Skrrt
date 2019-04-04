6422676077bde925cdbf81cc88e0d17915017efd
Craigybaeb <1806864@rgu.ac.uk>
2019-04-04 21:52:19 +0100

    db test

--- final/server.js
+++ final/server.js
@@ -25,12 +25,12 @@ app.post('/dologin', function(req, res) {
   var uname = req.body.uname;
   var pword = req.body.pword;
 
-  db.collection('people').findOne({"login.username":uname}, function(err, result) {
+  db.collection('profiles').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     //if(!result){res.redirect('/login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+    if(result.login.pword == pword){ req.session.loggedin = true; res.redirect('/') }
     //otherwise send them back to login
     //else{res.redirect('/login')}
   });

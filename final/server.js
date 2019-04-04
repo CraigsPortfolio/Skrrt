02727d338ed0244a92a200bb7bfc83a7b2f94664
Craigybaeb <1806864@rgu.ac.uk>
2019-04-04 21:04:08 +0100

    Database

--- final/server.js
+++ final/server.js
@@ -19,3 +19,15 @@ app.get('/register', function(req, res) {
 
 app.listen(8080);
 console.log('8080 is the magic port');
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+var db;
+MongoClient.connect(url, function(err, database){
+ if(err) throw err;
+ db = database;
+ app.listen(8080);
+});

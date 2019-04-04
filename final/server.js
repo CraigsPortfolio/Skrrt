9a99bc93bd3b08029ca9cbb2749e886a14a52dd1
Craigybaeb <1806864@rgu.ac.uk>
2019-04-04 21:16:18 +0100

    Login db

--- final/server.js
+++ final/server.js
@@ -16,13 +16,28 @@ app.get('/register', function(req, res) {
  res.render('pages/register');
 });
 
+app.post('/dologin', function(req, res) {
+  console.log(JSON.stringify(req.body))
+  var uname = req.body.username;
+  var pword = req.body.password;
+  alert(uname + " " + pword);
+  // db.collection('people').findOne({"login.username":uname}, function(err, result) {
+  //   if (err) throw err;//if there is an error, throw the error
+  //   //if there is no result, redirect the user back to the login system as that username must not exist
+  //   if(!result){res.redirect('/login');return}
+  //   //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+  //   if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
+  //   //otherwise send them back to login
+  //   else{res.redirect('/login')}
+  // });
+});
 
 app.listen(8080);
 console.log('8080 is the magic port');
 
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/profiles";
-const express = require('express');
+//const express = require('express');
 const app = express();
 app.use(express.static('public'))
 var db;

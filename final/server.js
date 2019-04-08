a05eb1cc84dc434bcd6b27e9a080d173d8d99630
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-08 14:39:40 +0100

    asd

--- final/server.js
+++ final/server.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser'); //npm install body-parser
 
 // //this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
 app.use(session({
+    secret: 'keyboard cat',
+    name: cookie_name,
+    store: sessionStore, // connect-mongo session store
+    proxy: true,
     resave: true,
     saveUninitialized: true
 }));

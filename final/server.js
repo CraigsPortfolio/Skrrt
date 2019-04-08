0a0de8edd5bdc114d8f247cb75ae813d66e5d324
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-08 17:42:33 +0100

    asd

--- final/server.js
+++ final/server.js
@@ -78,7 +78,7 @@ app.post('/dologin', function(req, res) {
   console.log(JSON.stringify(req.body))
   var uname = req.body.uname;
   var pword = req.body.pword;
-
+  var invalid = "Wrong";
   db.collection('profiles').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
@@ -86,7 +86,7 @@ app.post('/dologin', function(req, res) {
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.pword == pword){console.log("CORRECT"); req.session.loggedin = true; res.redirect('/') }
     //otherwise send them back to login
-    else{console.log("INCORRECT"); res.render('partials/login', {invalid : "INVALID"});}
+    else{console.log("INCORRECT"); res.render('partials/login', {invalid : invalid});}
   });
 });
 

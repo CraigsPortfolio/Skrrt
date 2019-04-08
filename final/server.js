f0edd8e2915ed537c241ce687fff6d8b5c963d9c
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-08 14:58:42 +0100

    revert

--- final/server.js
+++ final/server.js
@@ -17,15 +17,6 @@ app.get('/register', function(req, res) {
  res.render('pages/register');
 });
 
-app.get('/doLogin', function(req, res) {
- db.collection('quotes').find().toArray(function(err, result) {
- if (err) throw err;
- for (var i = 0; i < result.length; i++) {
- alert("Username = " + result[i].username);
- }
- res.send(output);
-});
-});
 
 app.listen(8080);
 console.log('8080 is the magic port');

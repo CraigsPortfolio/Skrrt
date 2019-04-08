d7d9ff43c8a1464eb12af561fc1bc67a0a36853d
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-08 14:08:47 +0100

    revert

--- final/server.js
+++ final/server.js
@@ -1,31 +1,8 @@
 var express = require('express');
 var app = express();
-const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
-const url = "mongodb://localhost:27017/profiles";
-const session = require('express-session'); //npm install express-session
-const bodyParser = require('body-parser'); //npm install body-parser
-
-// //this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
-// app.use(session({ secret: 'example' }));
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-
-var db;
-
-
-//this is our connection to the mongo db, ts sets the variable db as our database
-MongoClient.connect(url, function(err, database) {
-  if (err) throw err;
-  db = database;
-  app.listen(8080);
-  console.log('listening on 8080');
-});
-
 
 app.get('/', function(req, res) {
  res.render('pages/index');
@@ -42,19 +19,3 @@ app.get('/register', function(req, res) {
 
 app.listen(8080);
 console.log('8080 is the magic port');
-
-app.post('/dologin', function(req, res) {
-  console.log(JSON.stringify(req.body))
-  var uname = req.body.uname;
-  var pword = req.body.pword;
-
-  db.collection('profiles').findOne({"login.username":uname}, function(err, result) {
-    if (err) throw err;//if there is an error, throw the error
-    //if there is no result, redirect the user back to the login system as that username must not exist
-    if(!result){res.redirect('/login');return}
-    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.pword == pword){ alert(result.login.pword); req.session.loggedin = true; res.redirect('/') }
-    //otherwise send them back to login
-    else{res.redirect('/login')}
-  });
-});

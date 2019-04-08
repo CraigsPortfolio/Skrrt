4b2f882785405b9fc788b884b92157c0b7714438
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-08 14:41:42 +0100

    revert

--- final/server.js
+++ final/server.js
@@ -1,36 +1,9 @@
 var express = require('express');
 var app = express();
-const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
-const url = "mongodb://localhost:27017/profiles";
-const session = require('express-session'); //npm install express-session
-const bodyParser = require('body-parser'); //npm install body-parser
 
-// //this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
-app.use(session({
-    secret: 'keyboard cat',
-    proxy: true,
-    resave: true,
-    saveUninitialized: true
-}));
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-
-var db;
-
-
-//this is our connection to the mongo db, ts sets the variable db as our database
-MongoClient.connect(url, function(err, database) {
-  if (err) throw err;
-  db = database;
-  app.listen(8080);
-  console.log('listening on 8080');
-});
-
 
 app.get('/', function(req, res) {
  res.render('pages/index');
@@ -44,20 +17,15 @@ app.get('/register', function(req, res) {
  res.render('pages/register');
 });
 
+app.get('/doLogin', function(req, res) {
+ db.collection('quotes').find().toArray(function(err, result) {
+ if (err) throw err;
+ for (var i = 0; i < result.length; i++) {
+ alert("Username = " + result[i].username);
+ }
+ res.send(output);
+});
+});
 
 app.listen(8080);
 console.log('8080 is the magic port');
-
-
-app.get('/doLogin', function(req, res) {
- db.collection('profiles').find().toArray(function(err, result) {
- if (err) throw err;
- var output = "<h1>All the quotes</h1>";
- for (var i = 0; i < result.length; i++) {
- output += "<div>"
- output += "<h3>" + result[i].pword + "</h3>"
- output += "</div>"
- }
- res.send(output);
- });
-  });

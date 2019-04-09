fb26b9e9264035666aa581e6ecb56d1fd4423f3c
1805675-CraigRobertson <1805675@rgu.ac.uk>
2019-04-09 22:24:26 +0100

    s

--- final/server.js
+++ final/server.js
@@ -45,6 +45,7 @@ app.get('/', function(req, res) {
 app.get('/main', function(req, res) {
  res.render('pages/main');
  console.log(req.body.Start)
+ res.render('pages/main', {Start:start, End:end, Distance:distance, fuelPrice:fuelPrice, carDetails:carDetails, Passengers:passengers})
 });
 
 app.post('/main', function(req, res){

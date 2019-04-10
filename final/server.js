fa54299115d5f5985860e366f71f82d18a3aeb05
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-10 16:59:37 +0100

    asdad

--- final/server.js
+++ final/server.js
@@ -104,7 +104,7 @@ app.post('/addcar', function(req, res) {
 
 app.post('/addjourney', function(req, res) {
  var query = { "login.username": currentUser };
- var newvalues = { $addToSet: {journeys:{start: req.body.Start, end: req.body.End, pass: req.body.passengers, reg: req.body.reg, fcost: req.body.fuelPrice, mpg: req.body.vehicleMPG, rec:req.body.recommendedPrice} }};
+ var newvalues = { $addToSet: {journeys:{start: req.body.Start, end: req.body.End, pass: req.body.passValue, reg: req.body.reg, fcost: req.body.fuelPrice, mpg: req.body.vehicleMPG, rec:req.body.recommendedPrice} }};
  db.collection('profiles').update(query,newvalues, function(err, result) {
  if (err) throw err;
  res.redirect('/main');

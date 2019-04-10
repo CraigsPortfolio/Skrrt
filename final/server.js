4b008772dcc53f8bdb095f9360719abd9d182d13
Craigybaeb <1806864@rgu.ac.uk>
2019-04-10 03:07:17 +0100

    sdg

--- final/server.js
+++ final/server.js
@@ -116,7 +116,7 @@ app.get('/adduser', function(req, res) {
 app.post('/addcar', function(req, res) {
  var query = { username: currentUser };
  var newvalues = { $addToSet: {cars:{make: req.body.make, model: req.body.model, year: req.body.year, reg: req.body.reg, ftype: req.body.ftype, mpg: req.body.mpg} }};
- db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+ db.collection('profiles').updateOne(query,newvalues, function(err, result) {
  if (err) throw err;
  res.redirect('/garage');
  });

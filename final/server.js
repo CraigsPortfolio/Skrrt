cd670773cc036662005e593b6c625bf8892a82e6
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 00:24:07 +0100

    sdfsd

--- final/server.js
+++ final/server.js
@@ -68,29 +68,29 @@ app.get('/adduser', function(req, res) {
 });
 
 
-app.post('/adduser', function(req, res) {
-  //check we are logged in
-//  if(!req.session.loggedin){res.redirect('/login');return;}
-
-  //we create the data string from the form components that have been passed in
-
-var datatostore = {
-"fname":req.body.fname,
-"surname":req.body.surname,
-"login":{"username":req.body.username,"pword":req.body.password},
- "car":{"make": req.body.make, "model": req.body.model, "year": req.body.year, "reg": req.body.reg, "ftype": req.body.ftype, "mpg": req.body.mpg}
-}
-
-
-//once created we just run the data string against the database and all our new data will be saved/
-  db.collection('profiles').save(datatostore, function(err, result) {
-    if (err) throw err;
-    console.log('saved to database')
-    //when complete redirect to the index
-    currentUser=datatostore.login.username;
-    res.redirect('/profile')
-  })
-});
+// app.post('/adduser', function(req, res) {
+//   //check we are logged in
+// //  if(!req.session.loggedin){res.redirect('/login');return;}
+//
+//   //we create the data string from the form components that have been passed in
+//
+// var datatostore = {
+// "fname":req.body.fname,
+// "surname":req.body.surname,
+// "login":{"username":req.body.username,"pword":req.body.password},
+//  "car":{"make": req.body.make, "model": req.body.model, "year": req.body.year, "reg": req.body.reg, "ftype": req.body.ftype, "mpg": req.body.mpg}
+// }
+//
+//
+// //once created we just run the data string against the database and all our new data will be saved/
+//   db.collection('profiles').save(datatostore, function(err, result) {
+//     if (err) throw err;
+//     console.log('saved to database')
+//     //when complete redirect to the index
+//     currentUser=datatostore.login.username;
+//     res.redirect('/profile')
+//   })
+// });
 
 app.get('/adduser', function(req, res) {
     db.collection('profiles').findOne({"login.username": req.body.username}, function(err, user){

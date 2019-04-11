b99c853bcf980075e785fc3f8aff4dcd9d758f3e
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 23:54:47 +0100

    sdfs

--- final/public/js/populate.js
+++ final/public/js/populate.js
@@ -1,12 +1,3 @@
 function populate(){
-  var ddlItems = document.getElementById("dropgarage");
-    var  itemArray = <%= options %>;
-
-      for (var i = 0; i < itemArray.length; i++) {
-        var opt = itemArray[i];
-        var el = document.createElement("option");
-        el.textContent = opt.reg;
-        el.value = opt.reg;
-        ddlItems.appendChild(el);
-      }
+  alert("sdasd");
 }

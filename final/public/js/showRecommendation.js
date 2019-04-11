a67f62b97fce09ef3c6007ba57050cbc987fc748
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 01:11:16 +0100

    sdsdf

--- final/public/js/showRecommendation.js
+++ final/public/js/showRecommendation.js
@@ -2,9 +2,8 @@
 //Using JQuery
 function showRecommendation(){
 $(document).ready(function(){ //Waiting until document is fully loaded
-  if($("Start").text !="" && $("End").text !="" && $("fuelPrice-box").text !="" && $("reg-box").text !=""){
+  if(document.getElementById("Start").innerHTML !="" && document.getElementById("End").innerHTML !="" !="" && document.getElementById("fuelPrice-box").innerHTML !="" && document.getElementById("reg").innerHTML !=""){
   $("#recommendation").show(); //Showing recommendation section
-  alert(document.getElementById("fuelPrice-box").innerHTML);
   //Scrolling to the recommendation
   $('html, body').animate({
         scrollTop: $("#7").offset().top

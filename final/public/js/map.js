f6acef8eafdbd8295ac73a7aa1d16baae9e1f2d1
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 22:03:18 +0100

    dsfd

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -143,16 +143,3 @@ function postJourney(){
     rec:recommendedPrice
   });
 }
-
-function populate(){
-  var ddlItems = document.getElementById("dropgarage"),
-      itemArray = <%= options %>;
-
-      for (var i = 0; i < itemArray.length; i++) {
-        var opt = itemArray[i];
-        var el = document.createElement("option");
-        el.textContent = opt.reg;
-        el.value = opt.reg;
-        ddlItems.appendChild(el);
-      }
-}

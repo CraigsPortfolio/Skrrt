7620d3412d7340706130812fbf6b196bc84dc37c
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 21:57:53 +0100

    sdfsd

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -144,6 +144,15 @@ function postJourney(){
   });
 }
 
-function abc(){
-  alert("hellO");
+function populate(){
+  var ddlItems = document.getElementById("dropgarage"),
+      itemArray = <%= options %>;
+
+      for (var i = 0; i < itemArray.length; i++) {
+        var opt = itemArray[i];
+        var el = document.createElement("option");
+        el.textContent = opt.reg;
+        el.value = opt.reg;
+        ddlItems.appendChild(el);
+      }
 }

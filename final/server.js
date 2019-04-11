971c67b266fdfa88a240bfff510080870c3e47fb
Craigybaeb <1806864@rgu.ac.uk>
2019-04-11 19:47:57 +0100

    dsffd

--- final/server.js
+++ final/server.js
@@ -59,7 +59,7 @@ app.get('/newcar', function(req, res) {
 app.get('/garage', function(req, res) {
   db.collection('profiles').find({"login.username":currentUser}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
-    res.render('pages/garage', {make:result.car[0].make, model:result.car[0].model, reg:result.car[0].reg, ftype:result.car[0].ftype, mpg:result.car[0].mpg, options:result.car.reg});
+    res.render('pages/garage', {make:result.car.make, model:result.car.model, reg:result.car.reg, ftype:result.car.ftype, mpg:result.car.mpg, options:result.car.reg});
   });
 });
 

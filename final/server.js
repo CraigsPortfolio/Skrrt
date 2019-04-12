6855ca2e048439d00f5b1e8735309a04b5dd8cd3
Craigybaeb <1806864@rgu.ac.uk>
2019-04-12 03:56:56 +0100

    sdfd

--- final/server.js
+++ final/server.js
@@ -61,7 +61,7 @@ app.get('/garage', function(req, res) {
     if (err) throw err;//if there is an error, throw the error
     if(result==null){res.render('pages/garageempty');}
     else{res.render('pages/garage', {make:result.car[0].make, model:result.car[0].model, reg:result.car[0].reg, ftype:result.car[0].ftype, mpg:result.car[0].mpg, options:result.car});}
-}
+});
   });
 });
 

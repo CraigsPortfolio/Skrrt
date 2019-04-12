f1d56a6c65f7054c9496d5cbd0b1479f671e3547
CRAIG ROBERTSON (1805675) <1805675@rgu.ac.uk>
2019-04-12 15:37:18 +0100

    d

--- final/server.js
+++ final/server.js
@@ -75,6 +75,7 @@ app.post('/refresh', function(req, res) {
   // db.collection('profiles').find({"login.username": "CraigRoberts0n"}, {car: {$elemMatch:{reg: "EM55 KEL"}}})
   db.collection('profiles').find({"login.username": currentUser}, {car: {$elemMatch:{reg: "EM55 KEL"}}} , function(err, result) {
     if (err) throw err;//if there is an error, throw the error
+    console.log(result.car[0].make);
     res.render('/garage', {make:result.car[0].make, model:result.car[0].model, reg:result.car[0].reg, ftype:result.car[0].ftype, mpg:result.car[0].mpg});
   });
   });

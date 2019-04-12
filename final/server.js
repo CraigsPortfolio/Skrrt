242e07e69b56c6b35ad96a47fe9ecc7579c68308
1805675-CraigRobertson <1805675@rgu.ac.uk>
2019-04-12 01:42:47 +0100

    redirect

--- final/server.js
+++ final/server.js
@@ -144,7 +144,7 @@ app.post('/dologin', function(req, res) {
     //if there is no result, redirect the user back to the login system as that username must not exist
     // if(result.login.pword != pword){console.log("INCORRECT"); res.redirect({logmsg:"Incorrect login details"},'/main')}
     // if(result.login.pword != pword){console.log("INCORRECT"); res.redirect('back'}
-    if(result.login.pword != pword){console.log("INCORRECT"); backURL=req.header('Referer'); var x = backURL + "#loginError";console.log(x);res.redirect(backURL);}
+    if(result.login.pword != pword){console.log("INCORRECT"); backURL=req.header('Referer'); var x = backURL + "#loginError";console.log(x);res.redirect(x);}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.pword == pword){console.log("CORRECT"); req.session.loggedin = true; currentUser=result.login.username; res.redirect('/profile') }
     //otherwise send them back to login

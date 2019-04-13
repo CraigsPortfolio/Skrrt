e58991347fbd3256b585dcca215a578ebf1769db
1805675-CraigRobertson <1805675@rgu.ac.uk>
2019-04-13 15:39:04 +0100

    fix

--- final/public/js/main.js
+++ final/public/js/main.js
@@ -23,8 +23,8 @@ $('#addj').click(function() {
 			if(data == ""){
 				alert("Please Log in");
 			} else {
-				var nyBox = document.getElementById('ny-box').value
-				if(nyBox == ""){
+				var jnyBox = document.getElementById('jny-box').value
+				if(jnyBox == ""){
 					alert("Please supply a journey name")
 				} else {
 					postJourney();

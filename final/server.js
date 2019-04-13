85bcebcb287ef2ea727fa5c90ef305ac94d6d487
Craigybaeb <1806864@rgu.ac.uk>
2019-04-13 02:00:41 +0100

    edited

--- final/server.js
+++ final/server.js
@@ -149,20 +149,20 @@ app.post('/addjourney', function(req, res) {
 
 app.post('/remcar', function(req, res) {
  var query = { "login.username": currentUser};
- var newvalues = { $set: { login:{username: req.body.username, pword:req.body.pword},fname:req.body.fname, surname:req.body.surname}};
+ var newvalues = { $pull: {car:{reg: req.body.reg}}};
  db.collection('profiles').update(query,newvalues, function(err, result) {
  if (err) throw err;
  console.log("del");
- res.redirect('/profile');
+ res.redirect('/garage');
  });
 });
 
 app.post('/editprofile', function(req, res) {
  var query = { "login.username": currentUser};
- var newvalues = { $pull: {car:{reg: req.body.reg}}};
+ var newvalues = { $set: { login:{username: req.body.username, pword:req.body.pword},fname:req.body.fname, surname:req.body.surname}};
  db.collection('profiles').update(query,newvalues, function(err, result) {
  if (err) throw err;
- console.log("edited");
+ console.log("updated");
  res.redirect('/profile');
  });
 });

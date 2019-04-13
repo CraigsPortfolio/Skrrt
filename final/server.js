729aa8bbb2e1bc9bbe9433f3726666fd71073074
Craigybaeb <1806864@rgu.ac.uk>
2019-04-13 03:26:32 +0100

    sdfsd

--- final/server.js
+++ final/server.js
@@ -154,7 +154,7 @@ app.post('/remcar', function(req, res) {
  if (err) throw err;
  console.log("del");
  });
- db.collection('profiles').findOne({"login.username": currentUser}}) , function(err, result) {
+ db.collection('profiles').findOne({"login.username": currentUser} , function(err, result) {
    if (err) throw err;//if there is an error, throw the error
    console.log(result.car[0].make)
    var data = {make:result.car[0].make, model:result.car[0].model, reg:result.car[0].reg, ftype:result.car[0].ftype, mpg:result.car[0].mpg, options:result.car};

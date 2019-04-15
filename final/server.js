cd1e7663bb88156cbb1bb6ebe0b35ce00b9a5513
1805675-CraigRobertson <1805675@rgu.ac.uk>
2019-04-15 11:33:16 +0100

    Login, direct to page you came from

--- final/server.js
+++ final/server.js
@@ -569,7 +569,7 @@ app.post('/dologin', function(req, res) {
       console.log("CORRECT");
       req.session.loggedin = true;
       currentUser = result.login.username;
-      res.redirect('/profile')
+      res.redirect('back')
     }
     //if there is no result, redirect the user back to the login system as that username must not exist
     else {

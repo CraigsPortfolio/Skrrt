84376098bac167911350d49be9ffcceb1fee5858
Craigybaeb <1806864@rgu.ac.uk>
2019-04-19 02:14:26 +0100

    k

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -445,27 +445,6 @@ slider2.oninput = function() {
   }
 }
 
-});
-
-//This function makes the calculation section visible
-//Using JQuery
-function showRecommendation(){
-$(document).ready(function(){ //Waiting until document is fully loaded
-  //Checking that the fields aren't blank
-  if(document.getElementById("Start").value !="" && document.getElementById("End").value !=""  && document.getElementById("fuelPrice-box").value !="" && document.getElementById("reg-box").value !=""){
-    //Fields are valid
-  $("#recommendation").show(); //Showing recommendation section
-  //Scrolling to the recommendation
-  $('html, body').animate({
-        scrollTop: $("#7").offset().top
-    }, 0);
-  }else{ //Fields are invalid
-    //Show error message
-    document.getElementById("msg-show").innerHTML = "Please make sure all data is entered correctly before calculating!";
-  }
-});
-}
-
 //Writing button handlers
 $('#sub1').click(function() {
 	T();
@@ -522,3 +501,23 @@ $('#recbtn').click(function() {
 	calculateJourney(); //Calculate the journey
 	return false;
 });
+});
+
+//This function makes the calculation section visible
+//Using JQuery
+function showRecommendation(){
+$(document).ready(function(){ //Waiting until document is fully loaded
+  //Checking that the fields aren't blank
+  if(document.getElementById("Start").value !="" && document.getElementById("End").value !=""  && document.getElementById("fuelPrice-box").value !="" && document.getElementById("reg-box").value !=""){
+    //Fields are valid
+  $("#recommendation").show(); //Showing recommendation section
+  //Scrolling to the recommendation
+  $('html, body').animate({
+        scrollTop: $("#7").offset().top
+    }, 0);
+  }else{ //Fields are invalid
+    //Show error message
+    document.getElementById("msg-show").innerHTML = "Please make sure all data is entered correctly before calculating!";
+  }
+});
+}

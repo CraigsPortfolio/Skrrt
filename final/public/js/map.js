046cd68148eea632c5a03793f355ef3219f9cd24
Craigybaeb <1806864@rgu.ac.uk>
2019-04-19 01:28:18 +0100

    v

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -403,28 +403,44 @@ function addCar(){
   })
 }
 
-// Get the modal
-var modal = document.getElementById('id01');
-document.getElementById('errorMSG').style.visibility = "hidden";
+//This JavaScript section changes the number of passengers selected being displayed
+//According to the passenger range slider
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+var slider = document.getElementById("myRange"); //Gets passenger slider element and stores it in local variable
+var output = document.getElementById("demo"); //Gets text element and stores it in local variable
+
+//Initialising the output
+output.innerHTML = "Number of passengers: " + slider.value;
+
+//Responding to manipulation of passenger slider
+slider.oninput = function() {
+  output.innerHTML = "Number of passengers: " + this.value;
 }
 
-function isValidPath(str, path) {
-  str = str.substring(str.indexOf('://') + 3);
-  str = str.substring(str.indexOf('#') + 1);
-  return (str.indexOf(path) == 0);
-}
+//This JavaScript section changes the profit multiplier selected being displayed
+//According to the profit range slider
 
-var url = window.location.href;
-$(document).ready(function(){
-  if (isValidPath(url, 'loginError') == true){
-    document.getElementById('id01').style.display = "block";
-    document.getElementById('errorMSG').style.visibility = "visible";
-    document.getElementById('errorMSG').style.color = "red";
+var slider2 = document.getElementById("myRange2"); //Gets profit slider element and stores it in local variable
+var output2 = document.getElementById("demo2"); //Gets text element and stores it in local variable
+
+//Initialising the output
+output2.innerHTML = "Break Even";
+
+//Responding to manipulation of profit slider
+slider2.oninput = function() {
+  if(this.value==1){
+    output2.innerHTML = "Break Even";
   }
-});
+  if(this.value==2){
+    output2.innerHTML = "Reasonable";
+  }
+  if(this.value==3){
+    output2.innerHTML = "Fair";
+  }
+  if(this.value==4){
+    output2.innerHTML = "Greedy";
+  }
+  if(this.value==5){
+    output2.innerHTML = "Taking The P**S";
+  }
+}

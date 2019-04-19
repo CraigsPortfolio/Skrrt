df1bae01bd29f2d5e29a20ee779b4058c8989c65
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-19 14:44:33 +0100

    asdsa

--- final/public/js/modal.js
+++ final/public/js/modal.js
@@ -1,45 +1,25 @@
-// Get the modal
-var modal = document.getElementById('id01');
-document.getElementById('errorMSG').style.visibility = "hidden";
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function isValidPath(str, path) {
-  str = str.substring(str.indexOf('://') + 3);
-  str = str.substring(str.indexOf('#') + 1);
-  return (str.indexOf(path) == 0);
-}
-
-var url = window.location.href;
-$(document).ready(function(){
-  if (isValidPath(url, 'loginError') == true){
-    document.getElementById('id01').style.display = "block";
-    document.getElementById('errorMSG').style.visibility = "visible";
-    document.getElementById('errorMSG').style.color = "red";
-  }
-});
-
-
-// Get the modal
-var modal2 = document.getElementById('id02');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal2.style.display = "none";
-    }
-}
-
-
-
-var url2 = window.location.href;
-$(document).ready(function(){
-  if (isValidPath(url, 'loginError') == true){
-    $.post('/logout');
-  }
-});
+// // Get the modal
+// var modal = document.getElementById('id01');
+// document.getElementById('errorMSG').style.visibility = "hidden";
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+//
+// function isValidPath(str, path) {
+//   str = str.substring(str.indexOf('://') + 3);
+//   str = str.substring(str.indexOf('#') + 1);
+//   return (str.indexOf(path) == 0);
+// }
+//
+// var url = window.location.href;
+// $(document).ready(function(){
+//   if (isValidPath(url, 'loginError') == true){
+//     document.getElementById('id01').style.display = "block";
+//     document.getElementById('errorMSG').style.visibility = "visible";
+//     document.getElementById('errorMSG').style.color = "red";
+//   }
+// });

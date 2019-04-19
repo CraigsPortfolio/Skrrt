08eaa9670fed04a70e3ac40d901ff5b9f66e4999
Craigybaeb <1806864@rgu.ac.uk>
2019-04-19 01:13:35 +0100

    revert

--- final/public/js/showRecommendation.js
+++ final/public/js/showRecommendation.js
@@ -3,7 +3,7 @@
 function showRecommendation(){
 $(document).ready(function(){ //Waiting until document is fully loaded
   //Checking that the fields aren't blank
-  if( $("#Start").val()!="" && $("#End").val() !=""  && $("#fuelPrice-box".)val()!="" && $("#reg-box").val()!=""){
+  if(document.getElementById("Start").value !="" && document.getElementById("End").value !=""  && document.getElementById("fuelPrice-box").value !="" && document.getElementById("reg-box").value !=""){
     //Fields are valid
   $("#recommendation").show(); //Showing recommendation section
   //Scrolling to the recommendation
@@ -12,7 +12,7 @@ $(document).ready(function(){ //Waiting until document is fully loaded
     }, 0);
   }else{ //Fields are invalid
     //Show error message
-    $("#msg-show").html("Please make sure all data is entered correctly before calculating!");
+    document.getElementById("msg-show").innerHTML = "Please make sure all data is entered correctly before calculating!";
   }
 });
 }

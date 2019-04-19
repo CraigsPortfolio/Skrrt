39257f859f3e24af0d4a7217a954c52a39d5be34
Craigybaeb <1806864@rgu.ac.uk>
2019-04-19 01:33:53 +0100

    Reducing number of js files

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -445,3 +445,22 @@ slider2.oninput = function() {
   }
 }
 });
+
+//This function makes the calculation section visible
+//Using JQuery
+function showRecommendation(){
+$(document).ready(function(){ //Waiting until document is fully loaded
+  //Checking that the fields aren't blank
+  if(document.getElementById("Start").value !="" && document.getElementById("End").value !=""  && document.getElementById("fuelPrice-box").value !="" && document.getElementById("reg-box").value !=""){
+    //Fields are valid
+  $("#recommendation").show(); //Showing recommendation section
+  //Scrolling to the recommendation
+  $('html, body').animate({
+        scrollTop: $("#7").offset().top
+    }, 0);
+  }else{ //Fields are invalid
+    //Show error message
+    document.getElementById("msg-show").innerHTML = "Please make sure all data is entered correctly before calculating!";
+  }
+});
+}

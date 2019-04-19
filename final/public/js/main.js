d791a6d506676a53c9ae0ef5d5bc1b9ec89aa527
CRAIG PIRIE (1806864) <1806864@rgu.ac.uk>
2019-04-19 13:08:54 +0100

    asd

--- final/public/js/main.js
+++ final/public/js/main.js
@@ -1,43 +1,5 @@
 
 'use strict';
-
-//Writing button handlers
-
-$('#sub1').click(function() {
-	T();
-	return false;
-});
-
-$('#sub2').click(function() {
-	getMPG();
-	return false;
-});
-
-$('#addj').click(function() {
-	$.get( "/userLoggedIn", function(data) {
-			if(data == ""){
-				document.getElementById("msg").innerHTML = "Please Login";
-			} else {
-				var jnyBox = document.getElementById('jny-box').value
-				if(jnyBox == ""){
-					document.getElementById("msg").innerHTML = "Please supply a journey name";
-				} else {
-					postJourney();
-				}
-      }});
-	return false;
-});
-
-$('#editbtn').click(function() {
-	editProfile();
-	return false;
-});
-
-$('#edit-switch').click(function(){
-	editSwitch();
-	return false;
-})
-
 //Check if the 'Remove Car' button is clicked on the 'Garage' page
 $('#delcar').click(function() {
 	//'Remove Car' is clicked
@@ -45,19 +7,7 @@ $('#delcar').click(function() {
 	return false;
 });
 
-//Check if the 'Remove Journey' button is clicked on the 'Saved Journeys' page
-$('#deljny').click(function() {
-	//'Remove Journey' is clicked
-	postRemJny(); //Run the script to remove the journey from the database
-	return false;
-});
 
-//Check if the 'Calculate Journey' button is clicked on the 'Main' page
-$('#recbtn').click(function() {
-	//'Calculate Journey' is clicked
-	calculateJourney(); //Calculate the journey
-	return false;
-});
 
 $(window).on('load', function() {
 	/*------------------

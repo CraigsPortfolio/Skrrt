a8515a7ea0de3a8824133f07fb1831376b515b1e
Craigybaeb <1806864@rgu.ac.uk>
2019-04-19 01:52:09 +0100

    k

--- final/public/js/map.js
+++ final/public/js/map.js
@@ -444,31 +444,7 @@ slider2.oninput = function() {
     output2.innerHTML = "Taking The P**S";
   }
 }
-// Get the modal
-var modal = document.getElementById('id01');
-document.getElementById('errorMSG').style.visibility = "hidden";
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-function isValidPath(str, path) {
-  str = str.substring(str.indexOf('://') + 3);
-  str = str.substring(str.indexOf('#') + 1);
-  return (str.indexOf(path) == 0);
-}
-
-var url2 = window.location.href;
-$(document).ready(function(){
-  if (isValidPath(url2, 'loginError') == true){
-    document.getElementById('id01').style.display = "block";
-    document.getElementById('errorMSG').style.visibility = "visible";
-    document.getElementById('errorMSG').style.color = "red";
-  }
-});
 });
 
 //This function makes the calculation section visible

b65a3ba10dde6542e83dd9eb3b682bb4ae652965
Craig Pirie <craigpirie@craigs-mbp.home>
2020-02-17 01:10:56 +0000

    Removed all unnecessary folders / files

--- prototype/js/map.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(57.119647,-2.139528),
-  zoom:13,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-var map2 = new google.maps.Map(document.getElementById("googleMap2"),mapProp);
-}
